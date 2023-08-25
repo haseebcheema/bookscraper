cda6656f67d2fc4b0dddbdecf6d952014cd41b13
haseebcheema <ihaseebtariq1@gmail.com>
2023-08-25 12:02:09 +0500

    stored scraped data in the items

--- bookscraper/spiders/books.py
+++ bookscraper/spiders/books.py
@@ -1,4 +1,5 @@
 import scrapy
+from bookscraper.items import BookItem
 
 
 class BooksSpider(scrapy.Spider):
@@ -27,16 +28,18 @@ class BooksSpider(scrapy.Spider):
     def parse_book_page(self, response):
         table_rows = response.css('table tr')
         category = response.css('ul.breadcrumb li')
+        book_item = BookItem()
         
-        yield{
-            'url': response.url,
-            'title': response.css('div.product_main h1::text').get(),
-            'category': category[2].css('li a::text').get(),
-            'product_type': table_rows[1].css('td::text').get(),
-            'price_encl_tax': table_rows[2].css('td::text').get(),
-            'price_incl_tax': table_rows[3].css('td::text').get(),
-            'tax': table_rows[4].css('td::text').get(),
-            'availability': table_rows[5].css('td::text').get(),
-            'num_reviews': table_rows[6].css('td::text').get(),
-            'star_rating': response.css('p.star-rating').attrib['class'],
-        }
+        
+        book_item['url']: response.url
+        book_item['title']: response.css('div.product_main h1::text').get()
+        book_item['category']: category[2].css('li a::text').get()
+        book_item['product_type']: table_rows[1].css('td::text').get()
+        book_item['price_encl_tax']: table_rows[2].css('td::text').get()
+        book_item['price_incl_tax']: table_rows[3].css('td::text').get()
+        book_item['tax']: table_rows[4].css('td::text').get()
+        book_item['availability']: table_rows[5].css('td::text').get()
+        book_item['num_reviews']: table_rows[6].css('td::text').get()
+        book_item['star_rating']: response.css('p.star-rating').attrib['class']
+        
+        yield BookItem
